7f4f9900023b95ad9e0a8c417568b399b55252c9
watsonthescot94 <dillypogo94@gmail.com>
2022-04-18 23:37:18 +0100

    import useStates

--- src/client/user/Register.js
+++ src/client/user/Register.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { makeStyles } from "@material-ui/core"
 import TextField from "@material-ui/core/TextField"
 import Card from '@material-ui/core/Card'
