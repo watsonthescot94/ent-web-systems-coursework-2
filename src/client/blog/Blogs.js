07545c1719dbbd17c9bab857a7a6752ae7ba9797
watsonthescot94 <dillypogo94@gmail.com>
2022-04-18 02:38:01 +0100

    Recent posts title fix

--- src/client/blog/Blogs.js
+++ src/client/blog/Blogs.js
@@ -149,7 +149,7 @@ export default function Blogs({match}){
               >
                 {most_recent_posts.map((recent_post => {
                   return <ListItem button>
-                    <ListItemText primary={recent_post.title} />
+                    <ListItemText primary={recent_post.content.title} />
                     </ListItem>
                 }))}
 
