211353dfbc5ca93b6f844e725863a4588a3c2694
watsonthescot94 <dillypogo94@gmail.com>
2022-04-18 22:01:47 +0100

    Register layout created

--- src/client/MainRouter.js
+++ src/client/MainRouter.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {Route, Switch} from 'react-router-dom'
 import Blogs from './blog/Blogs'
 import Home from './core/Home'
+import Register from './user/Register'
 
 const MainRouter = () => {
     return (<div>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/blogs/:blog_id" component={Blogs}/>
+        <Route path='/register' component={Register}/>
       </Switch>
     </div>)
 }
