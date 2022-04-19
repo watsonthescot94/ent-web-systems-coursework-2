b55b1ba86c8f94b177cc638cd9ea16f8598285f7
watsonthescot94 <dillypogo94@gmail.com>
2022-04-19 07:34:23 +0100

    save

--- src/server/controllers/blog.controller.js
+++ src/server/controllers/blog.controller.js
@@ -38,7 +38,7 @@ const update = async (req, res) => {
     console.log("update() executing");
     let blog = req.post;
     blog = extend(blog, req.body.blog)
-    await blog.update()
+    await blog.save()
     res.json(blog)
 
     /*if (req.body.hasOwnProperty("changeLike")) {
